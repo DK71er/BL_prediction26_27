db8ab4d7546f370f689fb0b28e0802a3aaecb764
DK71er <valentin.kalemba@outlook.de>
2026-08-14 01:40:46 +0200

    added evalution metric in xgboost_model.py

--- xgboost_model.py
+++ xgboost_model.py
@@ -6,6 +6,22 @@ from sklearn.metrics import root_mean_squared_error
 def eval_model():
     print('Evaluating model ...')
 
+    def featur_importances():
+        imp_home = pd.Series(model_home.feature_importances_, index=Xtrain.columns).sort_values(ascending=False)
+        imp_away = pd.Series(model_away.feature_importances_, index=Xtrain.columns).sort_values(ascending=False)
+        print(f'Feature-importance home-model: {imp_home}')
+        print(f'Feature-importance away-model: {imp_away}')
+
+    def rmse_baseline_real():
+        baseline_pred = [ytrain['GoalsFor_Home'].mean()] * len(yval)
+        print(f"Basleine rmse: {root_mean_squared_error(yval['GoalsFor_Home'],baseline_pred)}")
+        print(f"Home-model rmse: {root_mean_squared_error(yval['GoalsFor_Home'], model_home.predict(Xval))}")
+        print(f"Away-model rmse: {root_mean_squared_error(yval['GoalsFor_Away'], model_away.predict(Xval))}")
+
+    rmse_baseline_real()
+
+
+
 
 params_model = {'objective': 'count:poisson',
                 'n_estimators': 2000,
@@ -20,20 +36,12 @@ model_away = XGBRegressor(**params_model)
 
 model_home.fit(Xtrain, ytrain['GoalsFor_Home'],
                 eval_set=[(Xval, yval['GoalsFor_Home'])],
+                verbose= 0
                 )
 
 model_away.fit(Xtrain, ytrain['GoalsFor_Away'],
-                eval_set=[(Xval, yval['GoalsFor_Away'])])
+                eval_set=[(Xval, yval['GoalsFor_Away'])],
+                verbose=0
+                )
 
-imp_home = pd.Series(model_home.feature_importances_, index=Xtrain.columns).sort_values(ascending=False)
-imp_away = pd.Series(model_away.feature_importances_, index=Xtrain.columns).sort_values(ascending=False)
-
-#print(imp_home)
-#print(imp_away)
-
-#print(model_home.predict(Xval).mean())
-baseline_pred = [ytrain['GoalsFor_Home'].mean()] * len(ytest)
-rmse_baseline = root_mean_squared_error(ytest['GoalsFor_Home'], baseline_pred)
-
-print(baseline_pred)
-print(rmse_baseline)
+eval_model()
